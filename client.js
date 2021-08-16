8ce54903f3710f6f145da88f939fbe8023c47cb1
rewpt <zpermack@gmail.com>
2021-08-16 17:19:37 +0000

    hardcoded moves to write to the game server for experimentation

--- client.js
+++ client.js
@@ -7,6 +7,14 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: zev");
+    setTimeout(() => {conn.write("Move: up");}, 2000);
+    setTimeout(() => {conn.write("Move: left");}, 4000);
+    setTimeout(() => {conn.write("Move: left");}, 5000); 
+    setTimeout(() => {setInterval(() => {conn.write('Move: down')}, 1000)}, 6000);
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
